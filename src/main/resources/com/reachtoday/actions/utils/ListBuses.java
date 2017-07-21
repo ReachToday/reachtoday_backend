9ce653f920ceb143df45e01b106b072be9306623
harikishorekuppa <harikishore.kuppa@gmail.com>
2017-07-21 15:05:45 +0530

    converted into maven

--- src/main/resources/com/reachtoday/actions/utils/ListBuses.java
+++ src/main/resources/com/reachtoday/actions/utils/ListBuses.java
@@ -50,7 +50,7 @@ public class ListBuses extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		try {
-			
+			System.out.println("inside the listbuses");
 			response.setContentType("text/html;charset=UTF-8");
 			PrintWriter out = response.getWriter();
 			
