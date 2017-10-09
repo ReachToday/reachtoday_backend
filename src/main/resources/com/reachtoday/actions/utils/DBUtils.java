8fae36b131690906611c485a113ed8e3c8395639
harikishorekuppa <harikishore.kuppa@gmail.com>
2017-10-09 19:54:59 +0530

    Updated the DButils for login and register user

--- src/main/resources/com/reachtoday/actions/utils/DBUtils.java
+++ src/main/resources/com/reachtoday/actions/utils/DBUtils.java
@@ -6,66 +6,78 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import org.json.JSONArray;
-
 public class DBUtils {
 
-	public Connection connect = null;
-	public Statement statement = null;
-	public ResultSet resultSet = null;
+  public Connection connect = null;
+  public Statement statement = null;
+  public ResultSet resultSet = null;
 
-	public Boolean	status=false;
+  public Boolean status = false;
 
-	public DBUtils() throws ClassNotFoundException, SQLException {
-		super();
+  public DBUtils() throws ClassNotFoundException, SQLException {
+    super();
+  }
 
-		// This will load the MySQL driver, each DB has its own driver
-		Class.forName("com.mysql.jdbc.Driver");
-		// Setup the connection with the DB
-		connect = DriverManager.getConnection("jdbc:mysql://192.168.2.195/reachtoday?user=root&password=soft123");
-		// Statements allow to issue SQL queries to the database
-		statement = connect.createStatement();
-	}
-	
-	 public ResultSet executeQuery(String query){
-		    try{
-		    	resultSet=statement.executeQuery(query);
-		    }catch(SQLException E){
-		      return null;
-		    }
-		    return resultSet;
+  public ResultSet executeQuery(String query) {
+    try {
+      // This will load the MySQL driver, each DB has its own driver
+      Class.forName("com.mysql.jdbc.Driver");
+      String url = "jdbc:mysql://localhost:3306/reachtoday";
+      String userName = "root";
+      String password = "Welcome@123";
+      // Setup the connection with the DB
+      connect = DriverManager.getConnection(url, userName, password);
+      // Statements allow to issue SQL queries to the database
+      statement = connect.createStatement();
+      resultSet = statement.executeQuery(query);
+    } catch (SQLException E) {
+      return null;
+    } catch (Exception E) {
+      return null;
     }
-	 
-	 public Boolean executeinsert(String query){
-		    try{
-		    	status =statement.execute(query);
-		    }catch(SQLException E){
-		      return null;
-		    }
-		    return status;
- }
-	 
-	 
-	// You need to close the resultSet
-			public void close() {
-				try {
-					if (resultSet != null) {
-						resultSet.close();
-						resultSet=null;
-					}
+    return resultSet;
+  }
 
-					if (statement != null) {
-						statement.close();
-						statement = null;
-					}
+  public Boolean executeinsert(String query) {
+    try {
+      // This will load the MySQL driver, each DB has its own driver
+      Class.forName("com.mysql.jdbc.Driver");
+      String url = "jdbc:mysql://localhost:3306/reachtoday";
+      String userName = "root";
+      String password = "Welcome@123";
+      // Setup the connection with the DB
+      connect = DriverManager.getConnection(url, userName, password);
+      // Statements allow to issue SQL queries to the database
+      statement = connect.createStatement();
+      status = statement.execute(query);
+    } catch (SQLException E) {
+      return null;
+    } catch (Exception E) {
+      return null;
+    }
+    return status;
+  }
 
-					if (connect != null) {
-						connect.close();
-						connect = null;
-					}
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
+  // You need to close the resultSet
+  public void close() {
+    try {
+      if (resultSet != null) {
+        resultSet.close();
+        resultSet = null;
+      }
+
+      if (statement != null) {
+        statement.close();
+        statement = null;
+      }
+
+      if (connect != null) {
+        connect.close();
+        connect = null;
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 
 }
